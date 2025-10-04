bd84ca419d01be3cdcbfb488c2420a79b1b8d717
Sri2292 <120458862+Sri2292@users.noreply.github.com>
2025-10-04 05:33:35 +0000

    Update build scripts for windows

--- build_main.sh
+++ build_main.sh
@@ -2,5 +2,14 @@
 # Build only the main application
 mkdir -p build
 cd build
-cmake ..
-make Basic_CPP_Project
+if [[ "$(uname -s)" == "Linux" ]]; then
+	cmake ..
+	make Basic_CPP_Project
+elif [[ "$(uname -s)" == "MINGW"* || "$(uname -s)" == "MSYS"* || "$(uname -s)" == "CYGWIN"* ]]; then
+	cmake .. -G "MinGW Makefiles"
+	mingw32-make Basic_CPP_Project
+else
+	echo "Please run the following commands manually for Windows Visual Studio:"
+	echo "cmake .. -G \"Visual Studio 17 2022\""
+	echo "cmake --build . --target Basic_CPP_Project"
+fi
--- build_tests.sh
+++ build_tests.sh
@@ -2,5 +2,14 @@
 # Build only the unit tests
 mkdir -p build
 cd build
-cmake ..
-make unit_tests
+if [[ "$(uname -s)" == "Linux" ]]; then
+	cmake ..
+	make unit_tests
+elif [[ "$(uname -s)" == "MINGW"* || "$(uname -s)" == "MSYS"* || "$(uname -s)" == "CYGWIN"* ]]; then
+	cmake .. -G "MinGW Makefiles"
+	mingw32-make unit_tests
+else
+	echo "Please run the following commands manually for Windows Visual Studio:"
+	echo "cmake .. -G \"Visual Studio 17 2022\""
+	echo "cmake --build . --target unit_tests"
+fi
